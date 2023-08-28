0cdd8dcd087dfd7af0e62df0eab2b3e7201c8d78
zhao-qingquan <92248168+zhao-qingquan@users.noreply.github.com>
2023-08-28 14:17:08 +0800

    Add files via upload

--- entry.py
+++ entry.py
@@ -51,40 +51,7 @@ class Logger(object):
     def flush(self):
         pass
 
-# if __name__ == '__main__':
-#     config_path = r'.\config.json'
-#     args, config = prepare(config_path)
-#     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
-#     path = os.path.abspath(os.path.dirname(__file__))
-#     type = sys.getfilesystemencoding()
-#     current_date = datetime.now().date()
 
-#     date_string = current_date.strftime("%Y-%m-%d")
-#     path=r'E:\Result\PTUPCDR\%s.txt'%date_string
-#     path_result=r'E:\Result\PTUPCDR\%s_result.txt'%date_string
-#     sys.stdout = Logger(path)
-
-#     if args.process_data_mid:
-#         for dealing in ['Books', 'CDs_and_Vinyl', 'Movies_and_TV']:
-#             DataPreprocessingMid(config['root'], dealing).main()
-#     if args.process_data_ready:
-#         for ratio in [[0.8, 0.2], [0.5, 0.5], [0.2, 0.8]]:
-#             for task in ['1', '2', '3']:
-#                 DataPreprocessingReady(config['root'], config['src_tgt_pairs'], task, ratio).main()
-#     print('task:{}; model:{}; ratio:{}; epoch:{}; lr:{}; gpu:{}; seed:{};'.
-#           format(args.task, args.base_model, args.ratio, args.epoch, args.lr, args.gpu, args.seed))
-#     for config['task'] in ['1','2','3']:
-#         for config['ratio'] in [0.2,0.5,0.8]:
-#             print("task:"+config["src_tgt_pairs"][config['task']]['src']+" and "+config["src_tgt_pairs"][config['task']]["tgt"])
-#             print("Ration:"+str(config['ratio']))
-#             print("lr:"+str(config['lr']))
-#             config['epoch']=70
-#             print('epoch：'+str(config['epoch']))
-#             run = Run(config)
-#             run.main()
-
-    # if not args.process_data_mid and not args.process_data_ready:
-    #     Run(config).main()
 if __name__ == '__main__':
     config_path = 'config.json'
     args, config = prepare(config_path)
